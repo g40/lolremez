e23ef24ca4c5548a53947418d61a375d604ba03c
J Evans <g4@novadsp.com>
2018-06-18 16:44:15 +0100

    Update to latest lolremez. Add build instructions. Update generate script.

--- src/lolremez.cpp
+++ src/lolremez.cpp
@@ -1,7 +1,7 @@
 //
 //  LolRemez — Remez algorithm implementation
 //
-//  Copyright © 2005—2018 Sam Hocevar <sam@hocevar.net>
+//  Copyright © 2005—2017 Sam Hocevar <sam@hocevar.net>
 //
 //  This program is free software. It comes without any warranty, to
 //  the extent permitted by applicable law. You can redistribute it
@@ -48,7 +48,6 @@ static void usage()
     printf("Mandatory arguments to long options are mandatory for short options too.\n");
     printf("  -d, --degree <degree>      degree of final polynomial\n");
     printf("  -r, --range <xmin>:<xmax>  range over which to approximate\n");
-    printf("  -p, --precision <bits>     floating-point precision (default 512)\n");
     printf("      --progress             print progress\n");
     printf("      --stats                print timing statistics\n");
     printf("  -h, --help                 display this help and exit\n");
@@ -94,20 +93,25 @@ int main(int argc, char **argv)
     bool has_weight = false;
     bool show_stats = false;
     bool show_progress = false;
+	bool structs = false;
+	// dump coefficients in tabluar form
+	bool templated = false;
 
     remez_solver solver;
 
     lol::getopt opt(argc, argv);
-    opt.add_opt('h', "help",      false);
-    opt.add_opt('v', "version",   false);
-    opt.add_opt('d', "degree",    true);
-    opt.add_opt('r', "range",     true);
-    opt.add_opt('p', "precision", true);
-    opt.add_opt(200, "float",     false);
-    opt.add_opt(201, "double",    false);
+    opt.add_opt('h', "help",     false);
+    opt.add_opt('v', "version",  false);
+    opt.add_opt('d', "degree",   true);
+    opt.add_opt('r', "range",    true);
+    opt.add_opt(200, "float",    false);
+    opt.add_opt(201, "double",   false);
     opt.add_opt(202, "long-double", false);
-    opt.add_opt(203, "stats",     false);
-    opt.add_opt(204, "progress",  false);
+    opt.add_opt(203, "stats",    false);
+    opt.add_opt(204, "progress", false);
+    opt.add_opt(205, "calc",     true);
+	opt.add_opt('t', "template", false);
+	opt.add_opt('s', "structs", false);
 
     for (;;)
     {
@@ -124,18 +128,19 @@ int main(int argc, char **argv)
             solver.set_order(degree);
           } break;
         case 'r': { /* --range */
-            array<std::string> arg = lol::split(std::string(opt.arg), ':');
+            array<std::string> arg = lol::split(std::string(opt.arg),':');
             if (arg.count() != 2)
                 FAIL("invalid range");
             str_xmin = arg[0];
             str_xmax = arg[1];
           } break;
-        case 'p': { /* --precision */
-            int bits = atoi(opt.arg);
-            if (bits < 32 || bits > 65535)
-                FAIL("invalid precision %s", opt.arg);
-            real::DEFAULT_BIGIT_COUNT = (bits + 31) / 32;
-          } break;
+		case 't': 
+			templated = true;
+			break;
+		case 's': 
+			structs = true;
+			templated = false;
+			break;
         case 200: /* --float */
             mode = mode_float;
             break;
@@ -151,6 +156,16 @@ int main(int argc, char **argv)
         case 204: /* --progress */
             show_progress = true;
             break;
+        case 205: { /* --calc */
+            expression ex;
+            if (!ex.parse(opt.arg))
+                FAIL("invalid range syntax: %s", opt.arg);
+            if (!ex.is_constant())
+                FAIL("invalid range: expression must be constant");
+            ex.eval(real::R_0()).print(40);
+            printf("\n");
+            return EXIT_SUCCESS;
+          } break;
         case 'h': /* --help */
             usage();
             return EXIT_SUCCESS;
@@ -191,7 +206,7 @@ int main(int argc, char **argv)
 
     has_weight = (opt.index + 1 < argc);
 
-    if (!ex.parse(argv[opt.index]))
+	    if (!ex.parse(argv[opt.index]))
         FAIL("invalid function: %s", argv[opt.index]);
 
     /* Special case: if the function is constant, evaluate it immediately */
@@ -251,24 +266,69 @@ int main(int argc, char **argv)
         printf(" * with weight function g(x) = %s\n", argv[opt.index + 1]);
     printf(" * on interval [ %s, %s ]\n", str_xmin.c_str(), str_xmax.c_str());
     printf(" * with a polynomial of degree %d. */\n", p.degree());
-    printf("%s f(%s x)\n{\n", type, type);
-    for (int j = p.degree(); j >= 0; --j)
-    {
-        char const *a = j ? "u = u * x +" : "return u * x +";
-        if (j == p.degree())
-            printf("    %s u = ", type);
-        else
-            printf("    %s ", a);
-        p[j].print(digits);
-        switch (mode)
-        {
-            case mode_float: printf("f;\n"); break;
-            case mode_double: printf(";\n"); break;
-            case mode_long_double: printf("l;\n"); break;
-        }
-    }
-    printf("}\n");
+	{
+		if (structs)
+		{
+			printf("{ %d, %s, %s, {", p.degree(), str_xmin.c_str(), str_xmax.c_str());
+		}
+		else if (templated)
+		{
+			type = "T";
+			printf("%s f_%d_%s_%s(int x)\n{\n", type, p.degree(), str_xmin.c_str(), str_xmax.c_str());
+		}
+		else
+		{
+			printf("%s f_%d_%s_%s(%s x)\n{\n", type, p.degree(), str_xmin.c_str(), str_xmax.c_str(), type);
+		}
+			
+		for (int j = p.degree(); j >= 0; --j)
+		{
+			if (structs)
+			{
+				p[j].print(digits);
+				switch (mode)
+				{
+				case mode_float: printf("f,"); break;
+				case mode_double: printf(","); break;
+				case mode_long_double: printf("l,"); break;
+				}
+			}
+			else if (templated)
+			{
+				char const *a = j ? "u = u * T(x) + T(" : "return u * T(x) + T(";
+				if (j == p.degree())
+					printf("    %s u = T(", type);
+				else
+					printf("    %s", a);
+				p[j].print(digits);
+				switch (mode)
+				{
+				case mode_float: printf("f);\n"); break;
+				case mode_double: printf(");\n"); break;
+				case mode_long_double: printf("l);\n"); break;
+				}
+			}
+			else
+			{
+				char const *a = j ? "u = u * x +" : "return u * x +";
+				if (j == p.degree())
+					printf("    %s u = ", type);
+				else
+					printf("    %s ", a);
+				p[j].print(digits);
+				switch (mode)
+				{
+				case mode_float: printf("f;\n"); break;
+				case mode_double: printf(";\n"); break;
+				case mode_long_double: printf("l;\n"); break;
+				}
+			}
+		}
+		if (structs)
+		printf("}},\n");
+		else
+			printf("}\n");
+	}
 
     return 0;
 }
-
